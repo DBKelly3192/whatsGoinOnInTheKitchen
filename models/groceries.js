b9270efcdd29604d4f443fcb19ac86434fd9c8c9
DBKelly3192 <DavidBenjaminKelly@gmail.com>
2020-10-25 10:54:47 -0400

    forgot to commit yesterday, created index, show, new, destroy routes

---
+++ models/groceries.js
@@ -0,0 +1,24 @@
+const groceries = [
+  {
+    name: 'milk',
+    price: 3,
+    preferredStore: 'Costco'
+  },
+  {
+    name: 'prosciutto',
+    price: 5,
+    preferredStore: 'Costco'
+  },
+  {
+    name: '1000 day old gouda',
+    price: 4,
+    preferredStore: 'Trader Joe\'s'
+  },
+  {
+    name: 'coffee',
+    price: 10,
+    preferredStore: 'Costco'
+  },
+]
+
+module.exports = groceries
--- server.js
+++ server.js
@@ -1,13 +1,49 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const methodOverride = require("method-override")
+const PORT = process.env.PORT
 
-const plants = ['Monstera Deliciosa', 'Corpse Flower', 'Elephant-Foot Yam', "Witches' Butter",];
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static("public"))
+app.use(methodOverride("_method"))
 
-app.get('/:indexOfPlantsArray', (req, res) => {
-    res.send(plants[req.params.indexOfPlantsArray]);
-});
+const groceries = require("./models/groceries.js")
 
-app.listen(port,() => {
-    console.log('listening on port' , port);
+app.use((req, res, next) => {
+  console.log("I run for all routes!")
+  next()
+})
+
+app.get("/groceries/new", (req, res) => {
+  res.render("groceries/newGroceries.ejs")
+})
+
+app.get("/groceries/", (req, res) => {
+  res.render("groceries/indexGroceries.ejs",
+  {
+    allGroceries: groceries
+  })
+})
+
+app.post("/groceries", (req, res) => {
+  groceries.push(req.body)
+  res.redirect("/groceries")
+})
+
+app.get("/groceries/:index", (req, res) => {
+  res.render("groceries/showGroceries.ejs",
+  {
+    groceryItem: groceries[req.params.index]
+  })
+})
+
+app.delete("/groceries/:index", (req, res) => {
+  groceries.splice(req.params.index, 1)
+  res.redirect("/groceries")
+})
+
+app.listen(PORT, () => {
+    console.log("listening on port" , PORT)
 });
