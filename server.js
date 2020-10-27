c0ac077b7d416440bde8a29754048e260d46478c
David Kelly <DavidBenjaminKelly@gmail.com>
2020-10-27 13:31:57 -0400

    deploying heroku

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-require('dotenv').config()
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 const methodOverride = require('method-override')
+require('dotenv').config()
+
+const app = express()
+
 const PORT = process.env.PORT
+const mongodbURI = process.env.MONGODBURI
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 
-const groceries = require('./models/groceries.js')
+// mongoose.connect(mongodbURI, {useNewUrlParser: true})
+mongoose.connect(mongodbURI,
+  {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+  }
+)
+mongoose.connection.once('open', ()=> {
+  console.log('connected to mongo')
+})
+
+// const groceries = require('./models/groceries.js')
+const GroceryItem = require('./models/groceries.js')
 
 app.use((req, res, next) => {
   console.log('I run for all routes!')
@@ -17,9 +34,14 @@ app.use((req, res, next) => {
 })
 
 app.get('/groceries/', (req, res) => {
-  res.render('groceries/indexGroceries.ejs',
-  {
-    allGroceries: groceries
+  // res.render('groceries/indexGroceries.ejs',
+  // {
+  //   allGroceries: groceries
+  // })
+  GroceryItem.find({}, (error, allGroceries) => {
+    res.render('groceries/indexGroceries.ejs', {
+      groceries: allGroceries
+    })
   })
 })
 
@@ -28,8 +50,12 @@ app.get('/groceries/new', (req, res) => {
 })
 
 app.post('/groceries', (req, res) => {
-  groceries.push(req.body)
-  res.redirect('/groceries')
+  // groceries.push(req.body)
+  // res.redirect('/groceries')
+  GroceryItem.create(req.body, (error, createdGroceryItem) => {
+    // res.send(createdGroceryItem)
+    res.redirect('/groceries')
+  })
 })
 
 app.get('/groceries/:index/edit', (req, res) => {
@@ -46,9 +72,12 @@ app.put('/groceries/:index', (req, res) => {
 })
 
 app.get('/groceries/:index', (req, res) => {
-  res.render('groceries/showGroceries.ejs',
-  {
-    groceryItem: groceries[req.params.index]
+  // res.render('groceries/showGroceries.ejs',
+  // {
+  //   groceryItem: groceries[req.params.index]
+  // })
+  Fruit.findById(req.params.id, (err, foundGroceryItem) => {
+    res.send(foundGroceryItem)
   })
 })
 
