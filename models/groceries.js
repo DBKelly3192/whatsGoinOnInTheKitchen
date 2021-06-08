ed0b537e960bb44a5b598573cc6dc8763eac03b2
David <DavidBenjaminKelly@gmail.com>
2021-06-08 12:03:00 -0400

    modified groceries model, added 'quantity' and 'quantityUnits' to schema

--- models/groceries.js
+++ models/groceries.js
@@ -5,6 +5,8 @@ const groceryItemSchema = new mongoose.Schema({
   brand: {type: String, required: false},
   price: {type: Number, required: false},
   preferredStore: {type: String, required: false},
+  quantity: {type: Number, required: false},
+  quantityUnit: {type: String, required: false},
   userID: {type: String, required: true}
 })
 
